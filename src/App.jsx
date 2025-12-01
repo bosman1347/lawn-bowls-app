6793e10ddd36b30f0745d14a0dbc45a8ba1e59b0
Bosman Olivier <bosman.olivier@gmail.com>
2025-12-01 13:32:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-
-import Home from "./pages/Home.jsx";
-import NewTournament from "./pages/NewTournament.jsx";
-import Matches from "./pages/Matches.jsx";
-import Scores from "./pages/Scores.jsx";
-import Results from "./pages/Results.jsx";
-import Standings from "./pages/Standings.jsx";
-import Summary from "./pages/Summary.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-
-
+import Dashboard from "./pages/Dashboard";
+import NewTournament from "./pages/NewTournament";
+import Matches from "./pages/Matches";
+import Standings from "./pages/Standings";
+import Summary from "./pages/Summary";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Router>
       <Navbar />
-
-      <div className="page-container">
-        <Routes>
-		  <Route path="/" element={<Dashboard />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<NewTournament />} />
-		  <Route path="/matches" element={<Matches />} />
-          <Route path="/scores" element={<Scores />} />
-          <Route path="/results" element={<Results />} />
-		  <Route path="/standings" element={<Standings />} />
-		  <Route path="/summary" element={<Summary />} />
-
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/new" element={<NewTournament />} />
+        <Route path="/matches" element={<Matches />} />
+        <Route path="/standings" element={<Standings />} />
+        <Route path="/summary" element={<Summary />} />
+      </Routes>
     </Router>
   );
 }
