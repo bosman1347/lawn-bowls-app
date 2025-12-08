107d2b1a1024e8cce32ca69b240a88d193c23a06
Bosman Olivier <bosman.olivier@gmail.com>
2025-12-08 15:59:11 +0200

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ export default function App() {
     <Router>
       <Navbar />
       <Routes>
+	    <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/tournaments" element={<Dashboard />} />
         <Route path="/new" element={<NewTournament />} />
