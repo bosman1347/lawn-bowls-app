db170e8844d71264e29a0cfd8e22ade9f9f52387
Bosman Olivier <bosman.olivier@gmail.com>
2025-12-08 17:06:58 +0200

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import NewTournament from "./pages/NewTournament";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import NewTournament from "./pages/NewTournament";
 import Matches from "./pages/Matches";
 import Standings from "./pages/Standings";
 import Summary from "./pages/Summary";
+
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Router>
       <Navbar />
+
       <Routes>
-        {/* HOME PAGE FIX */}
-        <Route path="/" element={<Dashboard />} />
+        {/* Home page now correctly uses Home.jsx */}
+        <Route path="/" element={<Home />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/tournaments" element={<Dashboard />} />
