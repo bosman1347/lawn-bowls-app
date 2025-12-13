3f282e13d4847c12cbb489e74c133469dd45d3f8
Bosman Olivier <bosman.olivier@gmail.com>
2025-12-13 13:16:16 +0200

    Reload auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,18 +2,18 @@
 
 const ADMIN_PIN = "9451";
 
-export function unlockAdmin(pin) {
+export const unlockAdmin = (pin) => {
   if (String(pin) === ADMIN_PIN) {
     localStorage.setItem("adminUnlocked", "yes");
     return true;
   }
   return false;
-}
+};
 
-export function isAdminUnlocked() {
+export const isAdminUnlocked = () => {
   return localStorage.getItem("adminUnlocked") === "yes";
-}
+};
 
-export function lockAdmin() {
+export const lockAdmin = () => {
   localStorage.removeItem("adminUnlocked");
-}
+};
