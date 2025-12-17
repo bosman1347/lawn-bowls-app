f7843093b661ea1fa789f09985c28500c14a28e3
Bosman Olivier <bosman.olivier@gmail.com>
2025-12-17 22:26:50 +0200

    Fix ProtectedPage.jsx to allow  player to enter scores

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -1,6 +1,6 @@
 import { useSearchParams } from "react-router-dom";
 import { isAdminUnlocked } from "../utils/auth";
-import PinEntry from "PinEntry";
+import PinEntry from "./components/PinEntry";
 
 export default function ProtectedPage({ children }) {
   const [params] = useSearchParams();
